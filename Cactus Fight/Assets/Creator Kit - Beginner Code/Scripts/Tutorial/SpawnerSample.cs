474bbc09f86d07f94dc7e8aa4b9f090c9c7107cf
WilIliam <long.le@indigames.net>
2022-04-26 10:35:55 +0700

    Introduction to the public keyword and inheritance

--- Cactus Fight/Assets/Creator Kit - Beginner Code/Scripts/Tutorial/SpawnerSample.cs
+++ Cactus Fight/Assets/Creator Kit - Beginner Code/Scripts/Tutorial/SpawnerSample.cs
@@ -3,23 +3,47 @@ using CreatorKitCode;
 
 public class SpawnerSample : MonoBehaviour
 {
-    public GameObject ObjectToSpawn;
+	public GameObject ObjectToSpawn;
 
-    int radius = 5;
+	int radius = 5;
 
-    void Start()
-    {
-        SpawnPotion(0);
-        SpawnPotion(45);
-        SpawnPotion(90);
-        SpawnPotion(135);
-    }
-
-    void SpawnPotion(int angle)
+	void Start()
 	{
-        Vector3 direction = Quaternion.Euler(0, angle, 0) * Vector3.right;
-        Vector3 spawnPosition = transform.position + direction * radius;
-        Instantiate(ObjectToSpawn, spawnPosition, Quaternion.identity);
-    }
+		LootAngle myLootAngle = new LootAngle(45);
+
+		//every call will advance the angle!
+		SpawnPotion(myLootAngle.NextAngle());
+		SpawnPotion(myLootAngle.NextAngle());
+		SpawnPotion(myLootAngle.NextAngle());
+		SpawnPotion(myLootAngle.NextAngle());
+	}
+
+	void SpawnPotion(int angle)
+	{
+		Vector3 direction = Quaternion.Euler(0, angle, 0) * Vector3.right;
+		Vector3 spawnPosition = transform.position + direction * radius;
+		Instantiate(ObjectToSpawn, spawnPosition, Quaternion.identity);
+
+	}
+
+	public class LootAngle
+	{
+		int angle;
+		int step;
+
+		public LootAngle(int increment)
+		{
+			step = increment;
+			angle = 0;
+		}
+
+		public int NextAngle()
+		{
+			int currentAngle = angle;
+			angle = Helpers.WrapAngle(angle + step);
+
+			return currentAngle;
+		}
+	}
 }
 
