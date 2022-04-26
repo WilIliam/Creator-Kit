589888f4e3f002a39ba2c17d93ffb20417b793ee
WilIliam <long.le@indigames.net>
2022-04-26 10:09:07 +0700

    Write your own instruction

--- Cactus Fight/Assets/Creator Kit - Beginner Code/Scripts/Tutorial/SpawnerSample.cs
+++ Cactus Fight/Assets/Creator Kit - Beginner Code/Scripts/Tutorial/SpawnerSample.cs
@@ -5,23 +5,25 @@ public class SpawnerSample : MonoBehaviour
 {
     public GameObject ObjectToSpawn;
 
+    int radius = 5;
+
     void Start()
     {
         int angle = 15;
         Vector3 spawnPosition = transform.position;
 
         Vector3 direction = Quaternion.Euler(0, angle, 0) * Vector3.right;
-        spawnPosition = transform.position + direction * 2;
+        spawnPosition = transform.position + direction * radius;
         Instantiate(ObjectToSpawn, spawnPosition, Quaternion.identity);
 
         angle = 55;
         direction = Quaternion.Euler(0, angle, 0) * Vector3.right;
-        spawnPosition = transform.position + direction * 2;
+        spawnPosition = transform.position + direction * radius;
         Instantiate(ObjectToSpawn, spawnPosition, Quaternion.identity);
 
         angle = 95;
         direction = Quaternion.Euler(0, angle, 0) * Vector3.right;
-        spawnPosition = transform.position + direction * 2;
+        spawnPosition = transform.position + direction * radius;
         Instantiate(ObjectToSpawn, spawnPosition, Quaternion.identity);
     }
 }
