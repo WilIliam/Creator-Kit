c9d53bcccf4703a43b4cd582bedc571d557ce50c
WilIliam <long.le@indigames.net>
2022-04-26 11:02:07 +0700

    Customize your game

---
+++ Cactus Fight/Assets/Creator Kit - Beginner Code/Scripts/EquippedEffect.cs
@@ -0,0 +1,47 @@
+/*
+* Create by William (c)
+* https://github.com/Long18
+*/
+using CreatorKitCode;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EquippedEffect : EquipmentItem.EquippedEffect
+{
+
+    #region Variables
+
+    #endregion
+
+    #region Unity Mehods
+
+    public override void Equipped(CharacterData user)
+    {
+
+    }
+
+    public override void Removed(CharacterData user)
+    {
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+    #endregion
+
+    #region Class
+
+    #endregion
+}
